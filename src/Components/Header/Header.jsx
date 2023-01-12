0b06dbf77a08f64bc01f907d9432de0102e70d51
waleed <waleed.sukhon77@gmail.com>
2023-01-13 00:20:34 +0200

    created files for components planning --Products related

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Logo from "./Logo/Logo";
+import "./style.css";
+export default function Header() {
+  return (
+    <header className="App-header">
+      <Logo />
+    </header>
+  );
+}
---
+++ src/Components/Header/TopItems/TopItems.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+export default function TopItems() {
+  return (
+    <div id="top-items">
+      <div id="top-item-1"></div>
+      <div id="top-item-2"></div>
+      <div id="top-item-3"></div>
+    </div>
+  );
+}
