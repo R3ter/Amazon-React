c8efa8d8b4526cc9b334d9fe7228786a5af61b44
waleed <waleed.sukhon77@gmail.com>
2023-01-14 22:08:14 +0200

    created files for components planning --Products related

---
+++ src/Components/body/ImageGallary/ImageGallary.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function ImageGallary() {
+  return (
+    <div>
+      <img src="https://m.media-amazon.com/images/I/612zDGeN+sL._AC_SY450_.jpg" />
+    </div>
+  );
+}
---
+++ src/Components/body/ImageGallary/MainImage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function MainImage() {
+  return (
+    <div>
+      <img src="https://m.media-amazon.com/images/I/612zDGeN+sL._AC_SY450_.jpg" />
+    </div>
+  );
+}
